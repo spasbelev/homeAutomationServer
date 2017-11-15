f585384fb2050c68e5e537b1e89e78cceeb1b18d
spasbelev <spasbelev@gmail.com>
2017-11-15 20:03:57 +0000

    Adding socket.io dependency and notifier to provide methods for clients

---
+++ server/notifier.js
@@ -0,0 +1,32 @@
+
+/*Define the listeners for our humidity and temperature.  */
+const listeners = {
+	temperature: [],
+	humidity: []
+}
+
+/* Arguments: callback function, type
+ * Brief: Pushes the provided listener to the array of listeners
+ * (either to temperature or humidity listener array depending on 
+ * "type" parameter */
+const subscribe = (listener, type) => {
+	listeners[type].push(listener)
+}
+
+/* Arguments: callback function, type
+ * Brief: Provides a way to unsubscribe(remove) a listener from 
+ * the subscribed array */
+const unsubscribe = (listenerToBeRemoved, type) => {}
+	listeners[type] = listeners[type].filter(listener =>
+		listener != listenerToBeRemoved)
+}
+
+const notify = (value, type) => {
+	listeners[type].forEach(listener => {
+		listener(value)
+	})
+}
+
+module.exports = {
+	subscribe, unsubsribe, notify
+}
