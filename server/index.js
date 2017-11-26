bb7d8c98f4a50427f2db4f95371d15bd5d60071c
Spas Belev <spas@Spass-MacBook-Pro.local>
2017-11-26 20:22:33 +0200

    Make server to be https and make login work

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const getCachedSensorReadings = require('./cache_sensor_data')
-const databaseOperations = require('./database-operations')
-const http = require('http')
-const socketIo = require('socket.io')
-const {subscribe, unsubscribe} = require('./notifier')
+const express = require('express');
+const path = require('path');
+const app = express();
+const getCachedSensorReadings = require('./cache_sensor_data');
+const databaseOperations = require('./database-operations');
+const https = require('https');
+const socketIo = require('socket.io');
+const {subscribe, unsubscribe} = require('./notifier');
+const loginValidate = require("./login/loginValidate");
+var fs = require('fs');
 
-const httpServer = http.Server(app)
+var key = fs.readFileSync('encryption/private.key');
+var cert = fs.readFileSync( 'encryption/primary.crt' );
+var ca = fs.readFileSync( 'encryption/intermediate.crt' );
 
-const io = socketIo(httpServer)
+var options = {
+	key: key,
+	cert: cert,
+	ca: ca
+  };
+
+const httpsServer = https.Server(app)
+
+const io = socketIo(httpsServer)
 
 io.on('connection', socket => {
 	console.log("User connected [${socket.id}]")
@@ -36,105 +48,19 @@ socket.on('disconnect', () => {
 	unsubscribe(pushHumidity, 'humidity')
 	})
 })
-httpServer.listen(3000, function () {
+httpsServer.listen(3000,'192.168.0.100', function () {
+	console.log(httpsServer.address().address)
 	console.log('Server listening on port 3000')
 })
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/login', express.static(path.join(__dirname, 'login')))
 
-app.use('/public/mainScreen', express.static(path.join(__dirname, 'public/mainScreen')))
-/*
-app.get('/temperature/history',function(req, res) {
-	databaseOperations.fetchLatestReading('temperature', 10, (err, result) => {
-if(err) {
-	console.error(err)
-	return res.status(500).end()
-}
-	res.json(result.reverse())
-	})
+//app.use('/public/mainScreen', express.static(path.join(__dirname, 'public/mainScreen')))
+
+app.post('/login', function(req, res, next) {
+	if(validateLogin()) {
+		res.redirect("/login")
+	} else {
+		res.redirect("./public/mainScreen/mainPage.html");
+	}
 })
-
-app.get('/humidity/history', function(req, res) {
-	databaseOperations.fetchLatestReading('humidity', 10, (err, result) => {
-		if(err) {
-			console.error(err)
-			return res.status(500).end()
-		}
-		res.json(result.reverse())
-	})
-})
-
-app.get('/temperature/range', function(req, res) {
-	const {start, end} = req.query
-	databaseOperations.fetchReadingsBetweenTime('temperature',
-	start, end, (err, results) => {
-		if(err) {
-			console.error(err)
-			return res.status(500).end()
-		}
-		res.json(results)
-	})
-})
-
-app.get('/temperature/average', function(req, res) {
-	const {start, end} = req.query
-	databaseOperations.fetchReadingsBetweenTime('temperature',
-	start, end, (err, results) => {
-		if(err) {
-			console.error(err)
-			return res.status(500).end()
-		}
-		res.json({
-			value: results['avg(value'].toFixed(1)
-		})
-	})
-})
-
-app.get('/humidity/range', function(req, res) {
-	const {start, end} = req.query
-	databaseOperations.fetchReadingsBetweenTime('humidity',
-	start, end, (err, results) => {
-		if(err) {
-			console.error(err)
-			return res.status(500).end()
-		}
-		res.json(results)
-	})
-})
-
-
-
-app.get('/humidity/average', function(req, res) {
-	const {start, end} = req.query
-	databaseOperations.fetchReadingsBetweenTime('humidity',
-	start, end, (err, results) => {
-		if(err) {
-			console.error(err)
-			return res.status(500).end()
-		}
-		res.json({
-			value: results['avg(value'].toFixed(1)
-		})
-	})
-})
-
-app.use('/public', express.static(path.join(__dirname, 'public')))
-
-app.get('/temperature', function(req, res) {
-	res.json ({
-		value:
-		getCachedSensorReadings.getTemperature().toFixed(1)
-	})
-})
-
-app.get('/humidity', function(req, res) {
-	res.json({
-		value:
-		getCachedSensorReadings.getHumidity().toFixed(1)
-	})
-})
-
-app.listen(3000, function(){
-	console.log('Server listening on port 3000');
-})
-*/
--- server/login/loginValidate.js
+++ server/login/loginValidate.js
@@ -1,28 +1,31 @@
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validateLogin() {
-    var username = document.getElementsByName("username").value;
-    var password = document.getElementsByName("password").value;
+    var username = document.getElementById("userName").value;
+    var password = document.getElementById("userPass").value;
 
     // Idea is that on first startup the username and password will be generated and will be read from a file that is hidden.
     if ( username == "username" && password == "password") {
         alert ("Login successfully");
         console.log("Success");
-        window.location = "mainScreen/mainPage.html"; // Redirecting to other page.
-        return false;
+        return true;
     }
     else { 
         attempt --;// Decrementing by one.
         alert("You have left "+attempt+" attempts left");
         // Disabling fields after 3 attempts.
-        if( attempt == 0) {
-            document.getElementById("username").disabled = true;
-            document.getElementById("password").disabled = true;
-            document.getElementById("submit").disabled = true;
-            return false;
-        }
-        window.location = "index.html";
+        checkIfLastAttempt();
+        window.location.href = "index.html";
     }   
+}
+
+function checkIfLastAttempt() {
+    if( attempt == 0) {
+        document.getElementById("username").disabled = true;
+        document.getElementById("password").disabled = true;
+        document.getElementById("submit").disabled = true;
+        return false;
+    }
 }
 
 module.exports = {
