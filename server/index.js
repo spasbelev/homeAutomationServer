971d5ebc371a95a20c52ee7ba17736018ef93f79
Spas Belev <spas@Spass-MacBook-Pro.local>
2017-12-06 20:41:22 +0200

    Fix redirect to mainScreen not loading with css and js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 const getCachedSensorReadings = require('./cache_sensor_data');
 const databaseOperations = require('./database-operations');
 const https = require('https');
@@ -11,6 +12,9 @@ const loginValidate = require("./login/loginValidate");
 
 const httpServer = http.Server(app);
 const io = socketIo(httpServer);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 io.on('connection', socket => {
 	console.log("User connected [${socket.id}]")
@@ -51,21 +55,11 @@ app.get("/", function(req, res) {
 
 app.use('/login', express.static(path.join(__dirname, 'login')))
 
-// app.use('./public/mainScreen/', express.static(path.join(__dirname, 'public/mainScreen')))
+app.use("/public/mainScreen", express.static(path.join(__dirname, 'public/mainScreen')));
 
 app.get("/public/mainScreen/", function(req, res) {
-	res.render()
-	res.sendFile(path.join(__dirname+'/public/mainScreen/mainPage.html'));
+	res.sendFile( __dirname + "/public/" + "mainScreen/" + "mainPage.html" );
 })
-
-// app.get('*', function(req, res) {
-// 	if(loginValidate.wasLoginSuccessful()) {
-// 		res.redirect("/login")
-// 	} else {
-// 		res.redirect("./public/mainScreen/");
-// 	}
-// })
-
 app.post('/login', function(req, res, next) {
 	if(loginValidate.wasLoginSuccessful()) {
 		res.redirect("/login")
